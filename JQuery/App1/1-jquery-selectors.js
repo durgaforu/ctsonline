22d86fe84aa0dfe7da4fc1affe4706a4b2fc480e
richardsforu <praveen.somireddy@gmail.com>
2017-12-13 16:31:17 +0530

    jQuery added

---
+++ JQuery/App1/1-jquery-selectors.js
@@ -0,0 +1,10 @@
+$(document).ready(
+    function(){
+       
+        //$('h1').text('jQuery has modified this text');
+       // $("#france,.promo").text('Hyderabad');
+
+        $('#destinations').find('li').text('Secunderabad');
+        
+    }
+);
