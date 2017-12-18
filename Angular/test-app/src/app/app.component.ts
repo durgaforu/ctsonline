3203f75e0c4af17fe68249d924159878f7381023
richardsforu <praveen.somireddy@gmail.com>
2017-12-18 16:59:49 +0530

    Angular2 is added

---
+++ Angular/test-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'praveen-start',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'application';
+  name:string='Praveen Reddy S';
+
+  
+}
